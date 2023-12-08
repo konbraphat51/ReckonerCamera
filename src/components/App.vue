9b40847fabf311e2e72636a867e7e5d984aa872c
konbraphat51 <konbraphat@gmail.com>
2023-12-08 14:07:00 +0900

    Add: empty vue space

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+    name: 'App',
+    components: {
+        "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options))
+    },
+    setup() {
+        //set up i18n
+        const { t } = VueI18n.useI18n()
+        return { t }
+    },
+})
+</script>
