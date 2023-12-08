de4aa094b3a48ca8de2a0b83d98717aa33e45216
konbraphat51 <konbraphat@gmail.com>
2023-12-08 19:29:33 +0900

    Test: max accel

--- src/components/Sensors/Accelerometer.vue
+++ src/components/Sensors/Accelerometer.vue
@@ -4,6 +4,10 @@
             x: {{ accelerometerData.x }} <br>
             y: {{ accelerometerData.y }} <br>
             z: {{ accelerometerData.z }} <br>
+
+            xMax: {{ accelerometerData.xMax }} <br>
+            yMax: {{ accelerometerData.yMax }} <br>
+            zMax: {{ accelerometerData.zMax }} <br>
         </p>
     </div>
 </template>
@@ -23,7 +27,10 @@ export default Vue.defineComponent({
             accelerometerData: {
                 x: 0,
                 y: 0,
-                z: 0
+                z: 0,
+                xMax: 0,
+                yMax: 0,
+                zMax: 0,
             },
             flagListening: false
         }
@@ -43,6 +50,16 @@ export default Vue.defineComponent({
             this.accelerometerData.x = event.acceleration.x
             this.accelerometerData.y = event.acceleration.y
             this.accelerometerData.z = event.acceleration.z
+
+            if (Math.abs(this.accelerometerData.x) > Math.abs(this.accelerometerData.xMax)) {
+                this.accelerometerData.xMax = this.accelerometerData.x
+            }
+            if (Math.abs(this.accelerometerData.y) > Math.abs(this.accelerometerData.yMax)) {
+                this.accelerometerData.yMax = this.accelerometerData.y
+            }
+            if (Math.abs(this.accelerometerData.z) > Math.abs(this.accelerometerData.zMax)) {
+                this.accelerometerData.zMax = this.accelerometerData.z
+            }
         },
     }
 })
