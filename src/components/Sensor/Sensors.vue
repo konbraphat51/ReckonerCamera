67eec21eb4131163d2a455a8a43b290846de58bb
konbraphat51 <konbraphat@gmail.com>
2023-12-08 16:39:30 +0900

    Add: basic sensors component

---
+++ src/components/Sensor/Sensors.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="Sensors">
+
+    </div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+    name: 'Sensors',
+    components: {
+    },
+    setup() {
+        //set up i18n
+        const { t } = VueI18n.useI18n()
+        return { t }
+    },
+})
+</script>
+
+<i18n>
+</i18n>
