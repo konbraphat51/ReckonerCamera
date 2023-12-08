c897b6527461d4accb5a04fc3cf9943eba56736e
konbraphat51 <konbraphat@gmail.com>
2023-12-08 17:03:49 +0900

    Add: sensors component to app

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,15 +2,20 @@
     <div id="app">
         <LanguageSelection />
         <Camera />
+        <Sensors />
     </div>
 </template>
 
 <script>
+import Sensors from './Sensor/Sensors.vue';
+
 export default Vue.defineComponent({
     name: 'App',
     components: {
         "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options)),
-        "Camera": Vue.defineAsyncComponent(() => loadModule("src/components/Camera/Camera.vue", options))
+        "Camera": Vue.defineAsyncComponent(() => loadModule("src/components/Camera/Camera.vue", options)),
+        "Sensors": Vue.defineAsyncComponent(() => loadModule("src/components/Sensor/Sensors.vue", options)),
+        Sensors
     },
     setup() {
         //set up i18n
