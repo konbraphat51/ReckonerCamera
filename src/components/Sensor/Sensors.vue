23ed23b27f909bdc26aa5c4431812c9b5fa408bf
konbraphat51 <konbraphat@gmail.com>
2023-12-08 16:55:58 +0900

    Add: request permission

--- src/components/Sensor/Sensors.vue
+++ src/components/Sensor/Sensors.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="Sensors">
-
+        <Accelerometer />
+        <PermissionRequester />
     </div>
 </template>
 
 <script>
+import Accelerometer from './Accelerometer.vue';
+
 export default Vue.defineComponent({
     name: 'Sensors',
     components: {
+        "Accelerometer": Vue.defineAsyncComponent(() => loadModule("src/components/Sensor/Accelerometer.vue", options)),
+        "PermissionRequester": Vue.defineAsyncComponent(() => loadModule("src/components/Sensor/PermissionRequester.vue", options)),
+        Accelerometer
     },
     setup() {
         //set up i18n
