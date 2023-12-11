7bc20869786174b952709c611e2043d29283681f
konbraphat51 <konbraphat@gmail.com>
2023-12-11 19:47:29 +0900

    Delete: Accelerometer

--- src/components/Sensors/Sensors.vue
+++ src/components/Sensors/Sensors.vue
@@ -1,7 +1,5 @@
 <template>
     <div id="Sensors">
-        <Accelerometer />
-        <PermissionRequester />
     </div>
 </template>
 
@@ -9,8 +7,6 @@
 export default Vue.defineComponent({
     name: 'Sensors',
     components: {
-        "Accelerometer": Vue.defineAsyncComponent(() => loadModule("src/components/Sensors/Accelerometer.vue", options)),
-        "PermissionRequester": Vue.defineAsyncComponent(() => loadModule("src/components/Sensors/PermissionRequester.vue", options)),
     }
 })
 </script>
