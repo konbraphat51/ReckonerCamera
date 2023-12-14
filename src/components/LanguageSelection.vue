598bb885a1e4d08f86db0148266e5ec3cb780511
konbraphat51 <konbraphat@gmail.com>
2023-12-14 15:33:05 +0900

    Add: language label

--- src/components/LanguageSelection.vue
+++ src/components/LanguageSelection.vue
@@ -1,12 +1,36 @@
 <template>
-    <select v-model="$i18n.locale">
-        <option value="en">English</option>
-        <option value="ja">日本語</option>
-    </select>
+	<div id="LanguageSelection">
+		{{ t("LanguageSelection.Language") }}
+
+		<select v-model="$i18n.locale">
+			<option value="en">English</option>
+			<option value="ja">日本語</option>
+		</select>
+	</div>
 </template>
 
 <script>
 export default {
-    name: 'LanguageSelection'
+	name: "LanguageSelection",
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
 }
 </script>
+
+<i18n>
+    {
+        "en": {
+            "LanguageSelection": {
+                "Language": "Language"
+            }
+        },
+        "ja": {
+            "LanguageSelection": {
+                "Language": "言語"
+            }
+        }
+    }
+</i18n>
