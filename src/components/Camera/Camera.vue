0790e45d4f03d3975d3cb30f05d0c07f2739a86a
konbraphat51 <konbraphat@gmail.com>
2023-12-14 15:20:59 +0900

    Refac

--- src/components/Camera/Camera.vue
+++ src/components/Camera/Camera.vue
@@ -1,47 +1,50 @@
 <template>
-    <div id="Camera">
-        <video ref="video" autoplay width="300" height="200"></video>
-        <Button @takePicture="takePicture" />
-    </div>
+	<div id="Camera">
+		<video ref="video" autoplay width="300" height="200"></video>
+		<Button @takePicture="takePicture" />
+	</div>
 </template>
 
 <script>
 export default Vue.defineComponent({
-    name: 'Camera',
-    components: {
-        "Button": Vue.defineAsyncComponent(() => loadModule("src/components/Camera/Button.vue", options))
-    },
-    setup() {
-        //set up i18n
-        const { t } = VueI18n.useI18n()
-        return { t }
-    },
-    mounted() {
-        const video = this.$refs.video
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(function (stream) {
-                    video.srcObject = stream;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        }
-    },
-    methods: {
-        takePicture() {
-            const canvas = document.createElement('canvas')
-            const video = this.$refs.video
-            canvas.width = video.videoWidth
-            canvas.height = video.videoHeight
-            const context = canvas.getContext('2d')
+	name: "Camera",
+	components: {
+		Button: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/Camera/Button.vue", options),
+		),
+	},
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+	mounted() {
+		const video = this.$refs.video
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+				.getUserMedia({video: true})
+				.then(function (stream) {
+					video.srcObject = stream
+				})
+				.catch(function (error) {
+					console.log(error)
+				})
+		}
+	},
+	methods: {
+		takePicture() {
+			const canvas = document.createElement("canvas")
+			const video = this.$refs.video
+			canvas.width = video.videoWidth
+			canvas.height = video.videoHeight
+			const context = canvas.getContext("2d")
 
-            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
-            const data = canvas.toDataURL('image/png')
-            this.$emit('pictureTaken', data)
+			context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
+			const data = canvas.toDataURL("image/png")
+			this.$emit("pictureTaken", data)
 
-            canvas.remove()
-        }
-    },
+			canvas.remove()
+		},
+	},
 })
 </script>
