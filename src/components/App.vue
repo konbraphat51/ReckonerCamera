322b93ef2e945212e44abd21ff52ef3c6f97e23f
konbraphat51 <konbraphat@gmail.com>
2023-12-14 15:34:55 +0900

    Refac

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,23 +1,34 @@
 <template>
-    <div id="app">
-        <LanguageSelection />
-        <Camera />
-        <Sensors />
-    </div>
+	<div id="app">
+		<LanguageSelection />
+		<Camera />
+		<Sensors />
+	</div>
 </template>
 
 <script>
 export default Vue.defineComponent({
-    name: 'App',
-    components: {
-        "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options)),
-        "Camera": Vue.defineAsyncComponent(() => loadModule("src/components/Camera/Camera.vue", options)),
-        "Sensors": Vue.defineAsyncComponent(() => loadModule("src/components/Sensors/Sensors.vue", options)),
-    },
-    setup() {
-        //set up i18n
-        const { t } = VueI18n.useI18n()
-        return { t }
-    },
+	name: "App",
+	components: {
+		LanguageSelection: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/LanguageSelection.vue", options),
+		),
+		Camera: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/Camera/Camera.vue", options),
+		),
+		Sensors: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/Sensors/Sensors.vue", options),
+		),
+	},
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+	methods: {
+		OnPictureTaken(data) {
+			console.log(data)
+		},
+	},
 })
 </script>
